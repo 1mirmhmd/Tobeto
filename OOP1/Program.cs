f54d3f7b263463b56d5a2ee4fdb79be17da4bd82
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-11-10 07:41:40 +0300

    OOP1 project added

---
+++ OOP1/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP1;
+
+Product product0 = new Product();
+product0.Id = 1;
+product0.CategoryId = 2;
+product0.ProductName = "Table";
+product0.UnitPrice = 250;
+product0.UnitsInStock = 10;
+Product product1 = new Product { Id = 2, CategoryId = 5, ProductName = "Pencil", UnitPrice = 10, UnitsInStock = 25 };
+
+// Instance Creation
+ProductManager productManager = new ProductManager();
+productManager.Add(product0);
+
+// int, double, bool ... value type
+// arrays, class, abstract class, interface ... reference type
+
