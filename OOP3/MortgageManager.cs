840db7b9923ee4d0a04e93aa4bd6612cd7615891
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-11-10 12:46:15 +0300

    Interface example added

---
+++ OOP3/MortgageManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class MortgageManager : ICreditManager
+    {
+        public void Calc()
+        {
+            Console.WriteLine("Konut Kredisi hesaplandı!");
+        }
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Oop2;
+
+// ind customer
+Individual customer = new Individual();
+customer.Id = 1;
+customer.FirstName = "Ali";
+customer.LastName = "Hancı";
+customer.TcNo = "12345678901";
+
+// corp customer
+Corporate corporate = new Corporate();
+corporate.Id = 2;
+corporate.CustomerNo = "123";
+corporate.TaxNo = "123456789";
+corporate.CompanyName = "Kodlama.io";
+
+// Individual Customer - Corporate 
+// SOLID
+
+Customer customer1 = new Individual();
+Customer customer2 = new Corporate();
+
+CustomerManager customerManager1 = new CustomerManager();
+customerManager1.Add(customer1);
