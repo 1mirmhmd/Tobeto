e29a807dddf72a414f5a1e1a770c9d89b9e138a0
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-11-10 12:52:34 +0300

    Example added

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using OOP3;
+
+// Class'lar ve interface'ler kendini implemente eden class'ın referans numarasını tutabilir
+ConsumerCreditManager consumerCreditManager = new ConsumerCreditManager();
+consumerCreditManager.Calc();
+
+MortgageManager mortgageManager = new MortgageManager();
+mortgageManager.Calc();
+
+ICreditManager vehicleCreditManager = new VehicleCreditManager();
+vehicleCreditManager.Calc();
